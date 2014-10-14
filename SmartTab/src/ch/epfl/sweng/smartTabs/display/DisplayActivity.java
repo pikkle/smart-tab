f83c9dbd45451899177c534bdfe4d637b71bc153
Faton Ramadani <faton.ramadani@gmail.com>
2014-10-14 14:02:30 +0200

    start display activity

---
+++ SmartTab/src/ch/epfl/sweng/smartTabs/display/DisplayActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.smartTabs.display;
+
+import ch.epfl.sweng.smartTabs.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display);
+	}
+}
