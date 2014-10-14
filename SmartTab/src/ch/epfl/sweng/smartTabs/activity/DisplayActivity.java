8098cc3ded322d18f935f5695fa8e3bc52aa59a3
Faton Ramadani <faton.ramadani@gmail.com>
2014-10-14 23:22:42 +0200

    start displayActivity

--- SmartTab/src/ch/epfl/sweng/smartTabs/activity/DisplayActivity.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/activity/DisplayActivity.java
@@ -1,5 +1,9 @@
 package ch.epfl.sweng.smartTabs.activity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -11,7 +15,10 @@ import android.view.View;
 
 @SuppressLint("DrawAllocation") 
 public class DisplayActivity extends Activity {
-
+	
+	private final float RATIO = 0.34375f;
+	private final char[] STANDARD_TUNNING = {'e','B','G','D','A','E'};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,17 +34,39 @@ public class DisplayActivity extends Activity {
         @Override
         protected void onDraw(Canvas canvas) {
            super.onDraw(canvas);
-           int x = getWidth();
-           int y = getHeight();
-           int radius;
-           radius = 100;
+
+           
            Paint paint = new Paint();
            paint.setStyle(Paint.Style.FILL);
            paint.setColor(Color.WHITE);
            canvas.drawPaint(paint);
-           // Use Color.parseColor to define HTML colors
-           paint.setColor(Color.parseColor("#CD5C5C"));
-           canvas.drawCircle(x / 2, y / 2, radius, paint);
+           drawNameSong(paint, canvas);
+           drawStrings(paint, canvas);
+           drawCursor(paint, canvas);
+       }
+        
+       private void drawNameSong(Paint paint, Canvas canvas){
+    	   paint.setTextSize(48f);
+           paint.setColor(Color.GRAY);
+           canvas.drawText("Jeux interdits", 50, 100, paint);
+       }
+       
+       private void drawStrings(Paint paint, Canvas canvas){
+           paint.setColor(Color.BLACK);
+           paint.setTextSize(36f);
+           paint.setStrokeWidth(2f);
+           for (int i = 0; i < 6; i++) {
+        	   canvas.drawLine(getWidth()/8, RATIO*getHeight() + i*getHeight()/16, getWidth(), RATIO*getHeight() + i*getHeight()/16, paint);
+        	   canvas.drawText(STANDARD_TUNNING[i]+"", getWidth()/16, RATIO*getHeight() + i*getHeight()/16, paint);
+           }
+           canvas.drawLine(getWidth()/8, RATIO*getHeight(), getWidth()/8, RATIO*getHeight() + 5*getHeight()/16, paint);
+       }
+       
+       private void drawCursor(Paint paint, Canvas canvas){
+    	   paint.setColor(Color.RED);
+    	   paint.setStrokeWidth(10f);
+    	   canvas.drawLine(getWidth()/3, getHeight()/4, getWidth()/3,3*getHeight()/4, paint);
+    	   paint.setStrokeWidth(1f);
        }
     }
 }
