c47b3aea3096b310c747cd09b1b0e1f43a1bd902
Ismail Imani <ismail.imani@epfl.ch>
2014-10-14 15:34:08 +0200

    Adding launcher and main attributes in Android_Manifest.xml

--- SmartTab/src/ch/epfl/sweng/smartTabs/activity/MainActivity.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/activity/MainActivity.java
@@ -2,14 +2,16 @@ package ch.epfl.sweng.smartTabs.activity;
 
 import ch.epfl.sweng.smartTabs.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent i = new Intent(this, DisplayActivity.class);
 	}
 }
