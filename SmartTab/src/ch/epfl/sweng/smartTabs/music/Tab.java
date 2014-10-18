c9a393f20aa99240c2ad8f24095b42975800e5ba
Raphael Khoury <raphael.362@gmail.com>
2014-10-18 14:08:47 +0200

    Almost final networking, TODO: Create menu from map

--- SmartTab/src/ch/epfl/sweng/smartTabs/music/Tab.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/music/Tab.java
@@ -1,6 +1,35 @@
 package ch.epfl.sweng.smartTabs.music;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+/**
+ *@author chrisgaubla
+ */
 public class Tab {
-	public String getName() {
-		return "Testing";
+	private final String tabName;
+	private final List<Time> timeList;
+	
+	private Tab(String aTabName, List<Time> aTimeList){
+		this.tabName = aTabName;
+		this.timeList = aTimeList;
 	}
+	
+	public static Tab parseTabFromJSON(JSONObject jsonTab){
+		return null;
+	}
+	
+	
+	
+	public String getTabName() {
+		return tabName;
+	}
+	public Time getTime(int index){
+		return timeList.get(index);
+	}
+	public int length(){
+		return timeList.size();
+	}
+
 }
