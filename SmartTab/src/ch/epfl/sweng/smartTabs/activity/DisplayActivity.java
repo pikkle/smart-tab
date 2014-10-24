bdfe5c7d7da61625ff4ac413788e81016aa72b33
Ismail Imani <ismail.imani@epfl.ch>
2014-10-24 12:31:55 +0200

    Implementing Automatic reader

--- SmartTab/src/ch/epfl/sweng/smartTabs/activity/DisplayActivity.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/activity/DisplayActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
-import ch.epfl.sweng.smartTabs.R;
 import ch.epfl.sweng.smartTabs.gfx.NoteView;
 import ch.epfl.sweng.smartTabs.gfx.TabAnimationThread;
 import ch.epfl.sweng.smartTabs.music.Instrument;
+import ch.epfl.sweng.smartTabs.music.SampleMap;
 import ch.epfl.sweng.smartTabs.music.Tab;
 import ch.epfl.sweng.smartTabs.music.Time;
 
@@ -17,18 +17,12 @@ import ch.epfl.sweng.smartTabs.music.Time;
  * The activity which displays the tabs and handle the animation
  */
 public class DisplayActivity extends Activity {
-	private static SoundPool pool;
+	private final int maxStreams = 45;
 
 	private static NoteView n;
-	private static int a3;
-	private static int b2;
-	private static int b3;
-	private static int e1;
-	private static int g2;
-	private static int g3;
-	private static int stringIndex;
-	private final int maxStreams = 6;
-
+	private static SoundPool pool;
+	private static SampleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,15 +33,8 @@ public class DisplayActivity extends Activity {
 
 		n = new NoteView(this, tab, Instrument.GUITAR);
 		pool = new SoundPool(maxStreams, AudioManager.STREAM_MUSIC, 0);
-
-		stringIndex = 1;
-
-		a3 = pool.load(this, R.raw.a3, 1);
-		b2 = pool.load(this, R.raw.b2, 1);
-		b3 = pool.load(this, R.raw.b3, 1);
-		e1 = pool.load(this, R.raw.e1, 1);
-		g2 = pool.load(this, R.raw.g2, 1);
-		g3 = pool.load(this, R.raw.g3, 1);
+		
+		map = new SampleMap(this, pool);
 
 
 		setContentView(n);
@@ -57,102 +44,11 @@ public class DisplayActivity extends Activity {
 	}
 
 	public static void playNote(Time time) {
-
-		switch (stringIndex) {
-		case 1:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b3, 1, 1, 1, 0, 1);
-			pool.play(e1, 1, 1, 1, 0, 1);
-
-			break;
-		case 2:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-			break;
-		case 3:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-
-			break;
-		case 4:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b3, 1, 1, 1, 0, 1);
-
-			break;
-		case 5:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-
-			break;
-		case 6:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-			break;
-
-		case 7:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b3, 1, 1, 1, 0, 1);
-
-			break;
-		case 8:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-
-			break;
-		case 9:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-
-			break;
-		case 10:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b3, 1, 1, 1, 0, 1);
-			pool.play(e1, 1, 1, 1, 0, 1);
-
-			break;
-		case 11:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-			break;
-		case 12:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-
-			break;
-		case 13:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(a3, 1, 1, 1, 0, 1);
-
-			break;
-		case 14:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-
-			break;
-		case 15:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-			break;
-
-		case 16:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g3, 1, 1, 1, 0, 1);
-
-			break;
-
-		case 17:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(b2, 1, 1, 1, 0, 1);
-
-			break;
-		case 18:
-			System.out.println("Playing string : " + stringIndex);
-			pool.play(g2, 1, 1, 1, 0, 1);
-
-			break;	
-		default:
-			break;
+		
+		for (int i = 0; i < 6; i++) {
+			if (!time.getNote(i).equals("")) {
+				pool.play(map.getSampleId(i, Integer.parseInt(time.getNote(i))), 1, 1, 1, 0, 1);
+			}
 		}
-		stringIndex++;
 	}
 }
