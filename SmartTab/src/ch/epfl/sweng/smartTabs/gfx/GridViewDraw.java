ee31c5f57cbd2411205562752e4985410d1b4789
Loic <loic.serafin@epfl.ch>
2014-10-27 17:26:58 +0100

    Starting partition drawing

--- SmartTab/src/ch/epfl/sweng/smartTabs/gfx/GridViewDraw.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/gfx/GridViewDraw.java
@@ -22,6 +22,8 @@ public class GridViewDraw extends Drawable{
 	private int mHeight;
 	private Instrument myInstrument;
 	private Tab myTab;
+	private boolean displayTab;
+	private boolean displayPartition;
 	
 	public GridViewDraw(int width, int height, Instrument instru, Tab tab) {
 		super();
@@ -36,15 +38,33 @@ public class GridViewDraw extends Drawable{
 	public void draw(Canvas canvas) {
 		clearScreen(canvas);
 		drawNameSong(canvas);
-		drawStrings(canvas);
+		
+		drawStrings(canvas, false);
+		drawStandardGrid(canvas, false);
+		
 		drawCursor(canvas);
 		
-		drawStandardGrid();
+		
 	}
 
-	private void drawStandardGrid() {
-		// TODO Auto-generated method stub
-		
+	private void drawStandardGrid(Canvas canvas, boolean centered) {
+		paint.setColor(Color.BLACK);
+		paint.setTextSize(36f);
+		paint.setStrokeWidth(2f);
+		if (centered){
+			for (int i = 0; i < 5; i++) {
+				canvas.drawLine(mWidth/10, ratio*mHeight + i*mHeight/20, mWidth, ratio*mHeight + i*mHeight/20, paint);
+				canvas.drawText(stantardTuning[i]+"", mWidth/16, ratio*mHeight + i*mHeight/16, paint);
+			}
+			canvas.drawLine(mWidth/10, ratio*mHeight, mWidth/10, ratio*mHeight + 5*mHeight/16, paint);
+		} else {
+			float distributedHeight = mHeight/8;
+			for (int i = 0; i < 5; i++) {
+				canvas.drawLine(mWidth/10, distributedHeight + i*mHeight/20, mWidth, distributedHeight + i*mHeight/20, paint);
+			}
+			paint.setStrokeWidth(15f);
+			canvas.drawLine(mWidth/10 + 2f, distributedHeight, mWidth/10 + 2f, distributedHeight + 4*mHeight/20, paint);
+		}
 	}
 
 	@Override
@@ -77,21 +97,31 @@ public class GridViewDraw extends Drawable{
 		canvas.drawText(myTab.getTabName(), 50, 100, paint);
 	}
 	
-	private void drawStrings(Canvas canvas) {
+	private void drawStrings(Canvas canvas, boolean centered) {
 		paint.setColor(Color.BLACK);
 		paint.setTextSize(36f);
 		paint.setStrokeWidth(2f);
-		for (int i = 0; i < myInstrument.getNumOfStrings(); i++) {
-			canvas.drawLine(mWidth/8, ratio*mHeight + i*mHeight/16, mWidth, ratio*mHeight + i*mHeight/16, paint);
-			canvas.drawText(stantardTuning[i]+"", mWidth/16, ratio*mHeight + i*mHeight/16, paint);
+		if (centered){
+			for (int i = 0; i < myInstrument.getNumOfStrings(); i++) {
+				canvas.drawLine(mWidth/10, ratio*mHeight + i*mHeight/16, mWidth, ratio*mHeight + i*mHeight/16, paint);
+				canvas.drawText(stantardTuning[i]+"", mWidth/16, ratio*mHeight + i*mHeight/16, paint);
+			}
+			canvas.drawLine(mWidth/10, ratio*mHeight, mWidth/10, ratio*mHeight + 5*mHeight/16, paint);
+		} else {
+			float distributedHeight = (mHeight-(mHeight/8))/2;
+			for (int i = 0; i < myInstrument.getNumOfStrings(); i++) {
+				canvas.drawLine(mWidth/10, distributedHeight + i*mHeight/16, mWidth, distributedHeight + i*mHeight/16, paint);
+				canvas.drawText(stantardTuning[i]+"", mWidth/16, distributedHeight + i*mHeight/16, paint);
+			}
+			canvas.drawLine(mWidth/10, distributedHeight, mWidth/10, distributedHeight + 5*mHeight/16, paint);
 		}
-		canvas.drawLine(mWidth/8, ratio*mHeight, mWidth/8, ratio*mHeight + 5*mHeight/16, paint);
+		
 	}
 
 	private void drawCursor(Canvas canvas) {
 		paint.setColor(Color.RED);
 		paint.setStrokeWidth(10f);
-		canvas.drawLine(mWidth/4, mHeight/4, mWidth/4,3*mHeight/4, paint);
+		canvas.drawLine(mWidth/4, mHeight/8, mWidth/4, 7*mHeight/8, paint);
 		paint.setStrokeWidth(1f);
 	}
 }
