e4a12f0699e462e70c3e4e0c9ab7dba46f2d370e
Loic <loic.serafin@epfl.ch>
2014-10-28 13:17:00 +0100

    Added the sub-boxes

--- SmartTab/src/ch/epfl/sweng/smartTabs/gfx/GridViewDraw.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/gfx/GridViewDraw.java
@@ -34,17 +34,22 @@ public class GridViewDraw extends Drawable{
 	private Resources mRes;
 	
 	private Rect screenRect; // full screen
-	private Rect leftPartRect;
 	private Rect headerRect; // title, ..
-	private Rect topScreenRect; // standard notation
-	private Rect bottomScreenRect; // tablature
+	private Rect bodyRect; // main content
+	private Rect leftPartRect; // music header (Clef, tuning)
+	private Rect rightPartRect;  // music content
+	private Rect topContentRect; // standard notation
+	private Rect bottomContentRect; // tablature
 	private Rect centerScreenRect; // standard/tablature notation only
 	private Rect leftTopPartRect; // Clef for the music sheet
 	private Rect leftBottomPartRect; // Tuning for the tablature
 	private Rect leftCenterPartRect; // Clef OR Tuning 
+	private Rect rightTopPartRect; // Clef for the music sheet
+	private Rect rightBottomPartRect; // Tuning for the tablature
+	private Rect rightCenterPartRect; // Clef OR Tuning 
 	
-	private static final float HEADER_PROPORTION = 0.1f;
-	private static final float LEFT_SIDE_PROPORTION = 0.25f;
+	private static final float HEADER_RATIO = 0.1f;
+	private static final float LEFT_SIDE_RATIO = 0.25f;
 	private static final float MARGIN = 0.125f;
 	
 	private Bitmap mGKeyImage;
@@ -58,19 +63,7 @@ public class GridViewDraw extends Drawable{
 		myTab = tab;
 		mRes = res;
 		
-		screenRect = new Rect(0, 0, mWidth, mHeight);
-		headerRect = new Rect(screenRect.left, screenRect.top, 
-				screenRect.right, (int) (screenRect.height() * HEADER_PROPORTION));
-		leftPartRect = new Rect(screenRect.left, headerRect.top, 
-				(int) (screenRect.right * LEFT_SIDE_PROPORTION), screenRect.bottom);
-		topScreenRect = new Rect(screenRect.left, headerRect.bottom, screenRect.right, screenRect.centerY());
-		bottomScreenRect = new Rect(screenRect.left, screenRect.centerY(), screenRect.right, screenRect.bottom);
-		centerScreenRect = new Rect(screenRect.left, headerRect.bottom, screenRect.right, screenRect.bottom); //TODO diviser le rectangle en hauteur pour avoir la même taille que le bottomScreenRect
-		leftTopPartRect = new Rect(leftPartRect.left, topScreenRect.top, leftPartRect.right, topScreenRect.bottom);
-		leftBottomPartRect = new Rect(leftPartRect.left, bottomScreenRect.top, 
-				leftPartRect.top, bottomScreenRect.bottom);
-		leftCenterPartRect = new Rect(leftCenterPartRect.left, centerScreenRect.top, 
-				leftCenterPartRect.right, centerScreenRect.bottom);
+		createBoxes();
 		
 		mGKeyImage = BitmapFactory.decodeResource(mRes, ch.epfl.sweng.smartTabs.R.raw.cledesol);
 		mGKeyImage = mGKeyImage.copy(Bitmap.Config.ARGB_8888, true);
@@ -79,6 +72,7 @@ public class GridViewDraw extends Drawable{
 		mGKeyImage = Bitmap.createScaledBitmap(mGKeyImage, newWidth, newHeight, false);
 	}
 
+	
 	@Override
 	public void draw(Canvas canvas) {
 		clearScreen(canvas);
@@ -178,4 +172,43 @@ public class GridViewDraw extends Drawable{
 		canvas.drawLine(mWidth/4, mHeight/8 + margin, mWidth/4, 15*mHeight/16, paint);
 		paint.setStrokeWidth(1f);
 	}
+	
+	/**
+	 * Initialize the screen boxes
+	 */
+	private void createBoxes() {
+		//TODO Consider margin within the sub boxes
+		// Divides the screen into 2 main boxes: header and body
+		screenRect = new Rect(0, 0, mWidth, mHeight);
+		headerRect = new Rect(screenRect.left, screenRect.top, 
+				screenRect.right, (int) (screenRect.height() * HEADER_RATIO));
+		bodyRect = new Rect(screenRect.left, headerRect.bottom, screenRect.right, screenRect.bottom);
+		
+		// Splits in two parts vertically
+		leftPartRect = new Rect(bodyRect.left, bodyRect.top, 
+				(int) (bodyRect.right * LEFT_SIDE_RATIO), bodyRect.bottom);
+		rightPartRect = new Rect(leftPartRect.right, bodyRect.top, bodyRect.right, bodyRect.bottom);
+		
+		// Splits the body in two parts horizontally or centers it
+		topContentRect = new Rect(bodyRect.left, bodyRect.bottom, bodyRect.right, bodyRect.centerY());
+		bottomContentRect = new Rect(bodyRect.left, bodyRect.centerY(), bodyRect.right, bodyRect.bottom);
+		int verticalMargin = (bodyRect.height()-bottomContentRect.height())/2;
+		centerScreenRect = new Rect(bodyRect.top+verticalMargin, bodyRect.left, 
+				bodyRect.bottom-verticalMargin, bodyRect.right);
+		
+		// Intersects the splits to get the left parts
+		leftTopPartRect = new Rect(leftPartRect.left, topContentRect.top, leftPartRect.right, topContentRect.bottom);
+		leftBottomPartRect = new Rect(leftPartRect.left, bottomContentRect.top, 
+				leftPartRect.right, bottomContentRect.bottom);
+		leftCenterPartRect = new Rect(leftPartRect.left, centerScreenRect.top, 
+				leftPartRect.right, centerScreenRect.bottom);
+		
+		// Intersects the splits to get the right parts
+		rightTopPartRect = new Rect(rightPartRect.left, topContentRect.top, rightPartRect.right, topContentRect.bottom);
+		rightBottomPartRect = new Rect(rightPartRect.left, bottomContentRect.top, 
+				rightPartRect.right, bottomContentRect.bottom);
+		rightCenterPartRect = new Rect(leftPartRect.left, centerScreenRect.top, 
+				rightPartRect.right, centerScreenRect.bottom);
+	}
+	
 }
