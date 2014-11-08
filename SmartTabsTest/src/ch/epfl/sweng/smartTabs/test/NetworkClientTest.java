0488eee992eb084de1a4b3da29ca03fa6ea53817
Raphael Khoury <raphael.362@gmail.com>
2014-11-08 20:30:32 +0100

    add Mockito Libraries to project

--- SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/NetworkClientTest.java
+++ SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/NetworkClientTest.java
@@ -14,7 +14,6 @@ import junit.framework.Assert;
 import org.json.JSONException;
 
 import android.test.AndroidTestCase;
-import android.view.ViewDebug.ExportedProperty;
 import ch.epfl.sweng.smartTabs.network.NetworkClient;
 
 
@@ -24,30 +23,31 @@ import ch.epfl.sweng.smartTabs.network.NetworkClient;
  * 212765
  */
 public class NetworkClientTest extends AndroidTestCase{
-	
+	NetworkClient netClient = new NetworkClient();
+
+
 	public void testJSONParsing() throws JSONException, MalformedURLException{
 		Map <String, URL> map = new HashMap<String, URL> ();
 		map.put("foo", new URL("http://bar.com/"));
 		String str = "[{\"name\":\"foo\",\"filename\":\"http://bar.com/\"}]";
-		NetworkClient netClient = new NetworkClient();
 		System.out.println(netClient.parseFromJson(str));
 		assertEquals(map, netClient.parseFromJson(str));
 	}
-	
+
 	public void testConnectionAndParsing() throws MalformedURLException, IOException{
 		//TODO: Input Test URL here.
-		final String url = "http://mpikkle.com/getTabs";
-		NetworkClient netClient = new NetworkClient();
+		final String url = "http://mpikkle.com/getTabsTest";
 		assertNotNull(netClient.downloadContent(new URL(url)));
-		
+
 		try{
 			netClient.downloadContent(new URL(""));
 			Assert.fail("Should throw MalformedURLException");
 		}
 		catch (MalformedURLException e){
-			
+
 		}
-		
+
 	}
-	
+
 }
+
