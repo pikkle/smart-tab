9529ad8b98da89a49644797b639c7e20b55029b7
Raphael Khoury <raphael.362@gmail.com>
2014-12-02 14:06:07 +0100

    small change in test

--- SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/TabTest.java
+++ SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/TabTest.java
@@ -16,7 +16,7 @@ import ch.epfl.sweng.smartTabs.music.Tab;
  * @author rphkhr Raphael El-Khoury 212765
  */
 public class TabTest extends AndroidTestCase {
-	public String goodTabString = "{ \"name\": \"foo\", \"complex\": false, \"tempo\": 120, \"partition\": [ { \"string_1\": \"0\", \"string_2\": \"1\", \"string_3\": \"2\", \"string_4\": \"2\", \"string_5\": \"0\", \"string_6\": \"0\"}]}";
+	public String goodTabString = "{ \"name\": \"foo\", \"complex\": false, \"tempo\": 120, \"partition\": [ { \"string_1\": \"0\", \"string_2\": \"1\", \"string_3\": \"2\", \"string_4\": \"2\", \"string_5\": \"0\", \"string_6\": \"0\", \"step\": 1}]}";
 
 	private List<String> jsonFields;
 
@@ -30,7 +30,6 @@ public class TabTest extends AndroidTestCase {
 		jsonFields.add("partition");
 	}
 
-	/*
 	public void testJSONParsing() throws JSONException {
 		JSONObject jObj = new JSONObject(goodTabString);
 		Tab parsedTab = Tab.parseTabFromJSON(jObj);
@@ -68,7 +67,7 @@ public class TabTest extends AndroidTestCase {
 			// success
 		}
 	}
-	*/
+
 	public void testMissingFields() throws JSONException {
 		for (String field : jsonFields) {
 			JSONObject jsonObject = new JSONObject(goodTabString);
