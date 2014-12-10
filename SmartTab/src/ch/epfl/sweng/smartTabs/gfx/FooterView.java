64e123cf55601a80c2b50c3c4d7ad294538da15f
Ismail Imani <ismail.imani@epfl.ch>
2014-12-10 23:02:38 +0100

    Removing unused variables and fixing warnings

--- SmartTab/src/ch/epfl/sweng/smartTabs/gfx/FooterView.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/gfx/FooterView.java
@@ -11,7 +11,6 @@ import android.view.View;
  */
 public class FooterView extends View {
 	private boolean displayed = true;
-	@SuppressWarnings("unused")
 	private Paint paint;
 	private Boolean running = false;
 
--- SmartTab/src/ch/epfl/sweng/smartTabs/gfx/TablatureView.java
+++ SmartTab/src/ch/epfl/sweng/smartTabs/gfx/TablatureView.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
-
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 import ch.epfl.sweng.smartTabs.music.Duration;
