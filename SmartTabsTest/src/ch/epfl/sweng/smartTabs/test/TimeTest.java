c0f407623387ed68300a16b0201210ae2dd95342
Raphael Khoury <raphael.362@gmail.com>
2014-12-11 18:54:45 +0100

    test changes

--- SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/TimeTest.java
+++ SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/TimeTest.java
@@ -10,7 +10,7 @@ import android.test.AndroidTestCase;
  */
 public class TimeTest extends AndroidTestCase{
 	String [] notes = {"7", "", "0"};
-	Time time = new Time(notes, null, "5" , 1, true, 2);
+	Time time = new Time(notes, null, "5" , 1);
 	
 	public void testNote(){
 	assertEquals(notes[0], time.getNote(0));
@@ -22,18 +22,6 @@ public class TimeTest extends AndroidTestCase{
 		assertEquals(5, time.getDuration());
 	}
 	*/
-	
-	public void testTernary(){
-		assertEquals(true, time.isTernary());
-	}
-	
-	public void testStep(){
-		assertEquals(2, time.getStep());
-	}
-	
-	public void testParsing(){
-		
-	}
 	
 	public void testMesure(){
 		assertEquals(1, time.getMesure());
