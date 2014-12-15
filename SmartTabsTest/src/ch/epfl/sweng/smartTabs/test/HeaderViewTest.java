c9948e762785016498ee9448f3bf88d025408e62
Faton Ramadani <faton.ramadani@epfl.ch>
2014-12-15 19:13:32 +0100

    cleaning test project

--- SmartTabsTest/src/ch/epfl/sweng/smartTabs/test/HeaderViewTest.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.sweng.smartTabs.test;
-
-import ch.epfl.sweng.smartTabs.gfx.HeaderView;
-import android.test.AndroidTestCase;
-
-public class HeaderViewTest extends AndroidTestCase {
-    HeaderView header =  new HeaderView(getContext(), "The Swag Man", "Ma Bentley");
-    
-}
