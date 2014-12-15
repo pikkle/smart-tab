46c162f3e8a3baf6a4ba9c8e7a7e2816e9188785
Faton Ramadani <faton.ramadani@epfl.ch>
2014-12-16 00:19:30 +0100

    OKLM

--- SmartTabTest/src/ch/epfl/sweng/smartTabs/ui/testMainActivity.java
+++ SmartTabTest/src/ch/epfl/sweng/smartTabs/ui/testMainActivity.java
@@ -4,18 +4,16 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import ch.epfl.sweng.smartTabs.activity.MainActivity;
 
-import com.google.android.apps.common.testing.ui.espresso.Espresso;
-import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
-import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
-import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 
 public class testMainActivity extends ActivityInstrumentationTestCase2<MainActivity>{
 
+    MainActivity a;
     public testMainActivity() {
         super(MainActivity.class);
     }  
     
     public void setUp() throws Exception {
-        getActivity();
-    }
+        super.setUp();
+        a = getActivity();
+    }   
 }
