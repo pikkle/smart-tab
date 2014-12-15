bc1b80d06669345e2165bc152adc4784bb299f09
Faton Ramadani <faton.ramadani@epfl.ch>
2014-12-15 20:40:28 +0100

    TURFU

--- SmartTabTest/src/ch/epfl/sweng/smartTabs/ui/testMainActivity.java
+++ SmartTabTest/src/ch/epfl/sweng/smartTabs/ui/testMainActivity.java
@@ -1,5 +1,21 @@
 package ch.epfl.sweng.smartTabs.ui;
 
-public class testMainActivity {
+import android.test.ActivityInstrumentationTestCase2;
 
+import ch.epfl.sweng.smartTabs.activity.MainActivity;
+
+import com.google.android.apps.common.testing.ui.espresso.Espresso;
+import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
+import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
+import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+
+public class testMainActivity extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    public testMainActivity() {
+        super(MainActivity.class);
+    }  
+    
+    public void setUp() throws Exception {
+        getActivity();
+    }
 }
